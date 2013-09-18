302c5a231d08f12060a770bc25c3316e8fd3a933
tjm7733 <tjm7733@rit.edu>
2013-09-18 19:51:33 -0400

    Added functionality to ImageManager

--- mvic/src/mivc/System/IO/ImageManager.java
+++ mvic/src/mivc/System/IO/ImageManager.java
@@ -4,6 +4,7 @@
 package mivc.System.IO;
 
 import java.io.File;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,19 +17,41 @@ public class ImageManager implements DataManager, Scannable<File> {
     protected ImageManager() { }
     
     public static ImageManager getInstance() { 
-	return instance; 
+        return INSTANCE; 
     }
 
     @Override
     public List<File> scan(String path) {
-	// TODO Scan path and return a List of paths to images
-	return null;
+        List<File> images = new LinkedList<File>();
+        File p = new File(path);
+        for (String s : p.list()) {
+            File f = (File) read(s);
+            if (f != null) {
+                images.add(f);
+            }
+        }
+        return images;
     }
 
     @Override
+    /**
+     * Read a non-hidden image file and return.
+     * 
+     * @param path - path to file
+     * @return File object related to the image
+     */
     public Object read(String path) {
-	// TODO Read an image from a file
-	return null;
+        File f = new File(path);
+        if (f.isDirectory() || f.isHidden()) {
+            return null;
+        }
+        
+        String extension = path.substring(path.lastIndexOf('.')).toLowerCase();
+        if (!isAcceptableExtension(extension)) {
+            return null;
+        }
+        
+        return f;
     }
 
     @Override
@@ -36,6 +59,19 @@ public class ImageManager implements DataManager, Scannable<File> {
 	// TODO write an image to a path.
 	
     }
+    
+    
+    private boolean isAcceptableExtension(String extension) {
+        for (String s : EXTENSIONS) {
+            if (s.equals(extension)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
 
-    private static ImageManager instance = new ImageManager();
+    private static final ImageManager INSTANCE = new ImageManager();
+    private static final String[] EXTENSIONS = new String[] 
+        {"jpeg", "jpg"};
 }
